6710bed59de5d670e31b76cb1789a3aeec8ae4fd
limweb@hotmail.com <limweb@hotmail.com>
2021-11-08 14:38:19 +0700

    add libs jwt and AuthController

---
+++ controller/AuthController.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+from typing import List
+import fastapi as _fastapi
+import sqlalchemy.orm as _orm
+from config import database as _dbservice
+
+_dbservice.create_database()
+
+router = APIRouter(
+    prefix="/api/auth",
+    tags=['auths']
+)
+
+@router.post("/signup")
+def sign_up() -> dict:
+    return { "msg": "singup" }
+
+@router.post("/signin")
+@router.post("/login")
+def sign_in():
+    return { "msg": "login" }
+
+@router.get("/signout")
+@router.get("/logout")
+def sign_out():
+    return { "msg": "logout" }
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from typing import List
 import fastapi as _fastapi
 import sqlalchemy.orm as _orm
 from config import database as _dbservice
-# from  controller import UserController as user_ctl, PostController as post_ctl
-from  controller import UserController as user_ctl , PostController as post_ctl
+import controller.UserController as UserController
+import controller.PostController as PostController
+import controller.AuthController as AuthController
 
 app = _fastapi.FastAPI()
 
@@ -13,5 +14,6 @@ _dbservice.create_database()
 def index() -> dict:
     return  {"message": "Fast Api Server v0.0.1"}
 
-app.include_router(user_ctl.router)
-app.include_router(post_ctl.router)
+app.include_router(AuthController.router)
+app.include_router(UserController.router)
+app.include_router(PostController.router)
